91451d63c3da6b08204716f212ad0b3e5d99a522
fernanda <fernandabitencourt@fernandas-mbp.home>
2022-03-31 20:48:35 +0100

    first login

--- config/database.js
+++ config/database.js
@@ -1,14 +1,12 @@
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
 require('dotenv').config();
-const MONGO_URL= process.env.MONGO_URL;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
-}
-).then(() => console.log('connection succesful'))
+}).then(() => console.log('connection succesful'))
   .catch((err) => console.error(err));
